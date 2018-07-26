88f22f8bb7d63fba00b73518a1cdc877c9e1c3f1
Rahly <karol.oleksy97@gmail.com>
2018-07-26 23:56:44 +0200

    Start to adding Bar menu and Item menu

--- java/home/expenses/controllers/MainController.java
+++ java/home/expenses/controllers/MainController.java
@@ -3,18 +3,27 @@ package home.expenses.controllers;
 import java.io.IOException;
 import java.util.ResourceBundle;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
+import static javafx.application.Application.STYLESHEET_CASPIAN;
+import static javafx.application.Application.STYLESHEET_MODENA;
+
+
 public class MainController {
+	
 	
 	@FXML
 	private BorderPane borderPane;
 	
 	@FXML 
 	private TopMenuButtonsController topMenuButtonsController;
+
 	
 	@FXML
 	private void initialize() {
@@ -34,5 +43,29 @@ public class MainController {
 			e.printStackTrace();
 		}
 		borderPane.setCenter(parent);
+	}
+	
+	@FXML
+	public void closeApplication(ActionEvent actionEvent) {
+		Platform.exit();
+		System.exit(0);
+	}
+	
+	@FXML
+	public void setCaspian(ActionEvent actionEvent) {
+		Application.setUserAgentStylesheet(STYLESHEET_CASPIAN);
+	}
+	
+	@FXML
+	public void setModena(ActionEvent actionEvent) {
+		Application.setUserAgentStylesheet(STYLESHEET_MODENA);
+	}
+	
+	@FXML
+	public  void setAlwaysOnTop(ActionEvent actionEvent) {
+	}
+	
+	@FXML
+	public void aboutApplication(ActionEvent actionEvent) {
 	}
 }
